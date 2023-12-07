da33ba6fa4f9166d010e21803dbed62d7abf8d37
Jed Macapagal <jedmacapagal@Jeds-Mac-mini.local>
2023-12-07 18:13:11 +0800

    Fixed line whitespaces

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,48 @@
-const codeEl = document.querySelectorAll("code");
-codeEl.forEach((el) => {
-    el.innerHTML = el.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-})
-hljs.highlightAll();
+shiki
+    .getHighlighter({
+        theme: "one-dark-pro",
+        langs: ["html"],
+    })
+    .then((highlighter) => {
+        const codeGroups = document.querySelectorAll(".code-group");
+
+        codeGroups.forEach((group) => {
+            const codeBlock = group.querySelector(".code-block");
+            const codeOutput = group.querySelector(".code-output");
+            const content = codeBlock.innerHTML;
+
+            const splittedLines = content.split("\n");
+            const dirtyLines = splittedLines.filter(line => {
+                if (!line.replace(/\s/g, '').length) return false;
+                return true;
+            });
+
+            let initialCount = 0;
+            const lines = dirtyLines.map((line, index) => {
+                let count = 0;
+                let html = "";
+            
+                for (let char of line) {
+                    if (char === " ") count++;
+                    else break;
+                }
+
+                if (index === 0) {
+                    initialCount = count;
+                }
+                
+
+                for (let i = 0; i < count - initialCount; i++) {
+                    html += " ";
+                }
+
+                html += line.trim();
+
+                return html;
+            });
+
+            const cleanedHTML = lines.join("\n");
+            const code = highlighter.codeToHtml(cleanedHTML, { lang: "html" });
+            codeOutput.innerHTML = code;
+        });
+    });
