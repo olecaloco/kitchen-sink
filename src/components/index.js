8302e183855b663a7bceb45aeda7bf43dcac7add
olecaloco <jedmacapagal@gmail.com>
2023-12-08 11:17:35 +0800

    Added Flex and Grid

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import BackButton from "./backBtn.astro";
 import CodeGroup from "./codeGroup.astro";
+import Navigation from "./navigation.astro";
 
-export {
-    BackButton,
-    CodeGroup
-}
+export { BackButton, CodeGroup, Navigation };
